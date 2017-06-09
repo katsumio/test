199352ffcd26b0c7bed5eb2d7e7e3f8b3d248387
katsumio <katsumi4023@gmail.com>
2017-06-09 16:50:18 +0900

    Python3におけるファイルの読み書き
    複数行の読み込み方法

--- open_1.py
+++ open_1.py
@@ -5,6 +5,7 @@
 # readlines()メソッド：複数行の読み込み
 # strip()メソッド：改行コードを取り除く
 #
+# バックスラッシュの入力方法：option + ¥
 # Create 2017/06/08
 # update 2017/06/08
 # Auther Katsumi.Oshiro
@@ -12,17 +13,22 @@
 # sample.txtが存在しない場合は、自動的に新規作成される。
 f = open('sample.txt','w')      # 'w'：上書きモード
 
-sample_str = 'Pythonは素晴しい言語です。'
+sample_str = 'Pythonは素晴しい言語です。\nTnsorFlowは素晴しい機械学習ライブラリです。'
 
+#
 # テキストファイルに書き込む
+#
 f.write(sample_str)
 f.flush()
 f.close()
 
+#
 # sample.txtを読み込みprintする
+#
 r = open('sample.txt','r')      # 'r'：読み込みモード
+sentence_list = r.readlines()
+r.close()
 
-readme = r.readline()           # readline()メソッドは１行だけ読み込み
-r.close
-
-print(readme)
+for sentence in sentence_list:
+    print(sentence)
+    print(sentence.strip())
