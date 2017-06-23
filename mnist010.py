81c4cf2f66a0462daf8aea7818e278515bc17747
katsumio <katsumi4023@gmail.com>
2017-06-23 23:17:37 +0900

    MNISTによる練習プログラムの作成
    MNISTによるTensorFlowのお勉強

--- mnist010.py
+++ mnist010.py
@@ -1,46 +1,61 @@
 # -*- coding: utf-8 -*-
 # (ハッシュマーク)コメントアウト記号
 # プログラミングの第一歩は"Hello World"を出力することですが
-# 機械学習では"MINST"(エムニスト)「手書き文字データ」です
+# 機械学習では"MNIST"(エムニスト)「手書き文字データ」です
+# MNIST:手書き文字の画像データセット
+# 28X28(784ユニット)のピクセルデータを入力して、10クラスの分類問題を解く
+# 訓練データ　：55,000（mnist.train）
+# テストデータ：10,000（mnist.test）
+# 検証データ　：5,000（mnist.validation）
 #
 # Create 2017/06/23
 # update 2017/06/23
 # Auther Katsumi.Oshiro
 
+# mnist手書きデータの用意（インポート）
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+# TensorFlowのインポート
 import tensorflow as tf
 
-x = tf.Variable(3, name='x')
-y = x * 5
+# 入力データの次元をセット[入力データ：数は任意,：784次元]
+x = tf.placeholder(tf.float32, [None, 784])
 
-sess = tf.InteractiveSession()
-sess.run(tf.global_variables_initializer())
-print(sess.run(y))
+#ニューラルネットのパラメータをセット
+w = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
 
-v = tf.Variable(3, name='v')
-v2 = v.assign(5)
-sess = tf.InteractiveSession()
-sess.run(v.initializer)
-print(sess.run(v))
-print(sess.run(v2))
+# 出力の設定
+y = tf.nn.softmax(tf.matmul(x, w) + b)
 
+# 正解ラベルの次元をセット
+y_ = tf.placeholder(tf.float32, [None, 10])
 
-'''
-with tf.name_scope('add_scope'):
-	x = tf.constant(1, name='x')
-	y = tf.constant(2, name='y')
-	z = x + y
+# 損失関数の定義
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 
-with tf.name_scope('multiply_scope'):
-	zz = y * z
+# 学習方法を決定
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
-with tf.name_scope('division_scope'):
-	yy = y / z
+# 初期化
+init = tf.global_variables_initializer()
 
-with tf.Session() as sess:
-	with tf.name_scope('init_scope'):
-		sess.run(tf.initialize_all_variables())
-	sess.run(zz)
-	# SummaryWriterで計算グラフを書く
-	summary_writer = tf.summary.FileWriter('data', graph=sess.graph)
-	tf.summary.scalar('one_plus_one_summary', zz)
-'''
+# 初期化の実行
+sess = tf.Session()
+sess.run(init)
+
+# 学習を実行
+for i in range(1000):
+	batch_xs, batch_ys = mnist.train.next_batch(100)
+	sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+# 制度の確認
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+
+# 出力と正解を調べた結果を獲得します
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+# 最後にテストデータでニューラルネットを評価
+print('ー正解ラベルとニューラルネットの予測を比較し正解率を表示ー')
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
